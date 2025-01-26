72037b3339376742b9d8a7dad660cbbce5d9b737
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2025-01-26 13:31:32 +0300

    Update password.py

--- password.py
+++ password.py
@@ -16,6 +16,8 @@ def generate_password(min_length, numbers=True, special_characters=True):
     meets_criteria = False
     has_number = False
     has_special = False
+    has_uppercase = False
+    has_lowercase = False
 
     while not meets_criteria or len(pwd) < min_length:
         new_char = random.choice(characters)
@@ -25,6 +27,10 @@ def generate_password(min_length, numbers=True, special_characters=True):
             has_number = True
         if new_char in special:
             has_special = True
+        if new_char.isupper():
+            has_uppercase = True
+        if new_char.islower():
+            has_lowercase = True
 
         meets_criteria = True
         if numbers:
@@ -34,11 +40,37 @@ def generate_password(min_length, numbers=True, special_characters=True):
 
     return pwd
 
+def evaluate_password_strength(password):
+    strength = 0
+  
+    if any(char.islower() for char in password):
+        strength += 1
+    if any(char.isupper() for char in password):
+        strength += 1
+    if any(char.isdigit() for char in password):
+        strength += 1
+    if any(char in string.punctuation for char in password):
+        strength += 1
+
+ 
+    if strength == 4:
+        return "Strong"
+    elif strength == 3:
+        return "Medium"
+    else:
+        return "Weak"
 
 
 min_length = int(input("Enter the minimum length of the password: "))
 include_numbers = input("Include numbers? (y/n): ").strip().lower() == "y"
 include_specials = input("Include special characters? (y/n): ").strip().lower() == "y"
 
+
 password = generate_password(min_length, include_numbers, include_specials)
+
+
+strength = evaluate_password_strength(password)
+
+
 print("The generated password is:", password)
+print("Password strength:", strength)
